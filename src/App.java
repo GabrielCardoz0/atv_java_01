b072ceef67ec99aec81298a8e2dc92c7ee22c169
Gabriel Cardozo <gabriel.scardozo7@gmail.com>
2023-06-13 15:27:30 -0300

    feat: atividade01

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int number = 2;
+
+        System.out.println("Tabela de multiplicação de: "+number);
+
+        for(int i = 1; i < 11; i++) {
+            System.out.println(i*number);
+        }
+
+        // for(int i = 1; i <= number; i++) {
+        //     String concNumber = "";
+        //     for(int j = 1; j <= i; j++) {
+        //         concNumber = concNumber+i;
+        //     }
+        //     System.out.println(concNumber);
+        // }
+
     }
 }
